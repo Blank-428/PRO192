a43e50a7f95a548c6f87b702415e1ed4c41b30cc
VNkiller <46163360+Blank-428@users.noreply.github.com>
2023-08-17 11:01:47 +0700

    VN

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Main{
     public static void main(String args[]){
-        System.out.println("điều khiển");
+        System.out.println("Việt Nam");
 
     Scanner scan= new Scanner(System.in);
 

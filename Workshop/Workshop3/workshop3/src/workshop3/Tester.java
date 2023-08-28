f41e30c79a3a2717026d847865237ed540d2b7c8
Blank.428 <46163360+Blank-428@users.noreply.github.com>
2023-08-28 19:14:22 +0700

    Update Workshop3

--- Workshop/Workshop3/workshop3/src/workshop3/Tester.java
+++ Workshop/Workshop3/workshop3/src/workshop3/Tester.java
@@ -1,8 +1,8 @@
 package workshop3;
 public class Tester {
     public static void main(String[] args) {
-        Guitar obj1=new Guitar();
-        Guitar obj2=new Guitar("G123",2000,"Sony","Model123","hardWood","softWood");
+        Guitar obj1 = new Guitar();
+        Guitar obj2 = new Guitar("G123",2000,"Sony","Model123","hardWood","softWood");
         System.out.println("State of obj1:");
         obj1.createSound();
         System.out.println("State of obj2:");

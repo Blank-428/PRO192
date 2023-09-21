921525f5d65274da1bdf94c2e30704e981aca99f
Blank.428 <46163360+Blank-428@users.noreply.github.com>
2023-09-21 14:42:10 +0700

    Update Workshop5

---
+++ Workshop/Workshop5/OrganizationManager/src/DTO/FPTUniversity.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DTO;
+
+/**
+ *
+ * @author admin
+ */
+public class FPTUniversity extends University implements Role{
+    private String address;
+
+    public FPTUniversity() {
+    }
+
+    public FPTUniversity(int size, String address, String name) {
+        super(size, name);
+        this.address = address;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    @Override
+    public void createWorker() {
+        System.out.println("providing human resources");
+    }
+
+    @Override
+    public String toString() {
+        return "FPTU has four campuses Hanoi, HCM, DaNang, CanTho, QuyNhon";
+    }
+}

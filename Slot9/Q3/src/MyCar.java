f4fd3ab077df0aa7cfcd811574a020ee6f681de7
Blank.428 <46163360+Blank-428@users.noreply.github.com>
2023-10-05 13:08:36 +0700

    Slot9 3/10

--- Slot9/Q3/src/MyCar.java
+++ Slot9/Q3/src/MyCar.java
@@ -16,14 +16,14 @@ import java.util.List;
  */
 public class MyCar implements ICar {
     
+    @Override
     public int f1(List<Car> t) {
         int sum = 0;
-        for (Car i: t) {
-            sum += i.getRate();
-        }
+        sum = t.stream().map((i) -> i.getRate()).reduce(sum, Integer::sum);
         return sum / t.size();
     }
     
+    @Override
     public void f2(List<Car> t) {
         int max = t.get(0).getRate();
         int vtMax = 0;
@@ -46,6 +46,7 @@ public class MyCar implements ICar {
         Collections.swap(t, vtMax, vtMin);
     }
     
+    @Override
     public void f3(List<Car> t) {
         for(int i = 0; i < t.size() - 1; i++) {
             for (int j = i + 1; j < t.size(); j++) {
